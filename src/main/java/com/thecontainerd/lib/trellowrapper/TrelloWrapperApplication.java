f0c6b5ccf6f81b7856d376d8051b827081331f0d
Rafael Jardim <rafael.jardim@camara.leg.br>
2022-03-17 15:36:43 -0300

    First commit

---
+++ src/main/java/com/thecontainerd/lib/trellowrapper/TrelloWrapperApplication.java
@@ -0,0 +1,13 @@
+package com.thecontainerd.lib.trellowrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloWrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloWrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thecontainerd/lib/trellowrapper/TrelloWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecontainerd.lib.trellowrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
